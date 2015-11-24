4a0b84c36a00b54fb1e4733621670d8a3aa14f0a
Javier Montero <javier1689@gmail.com>
2015-11-24 15:13:36 +0100

    Test preparation. Maybe Javier ( not me) will do it.

---
+++ test/main/SimulationTest.java
@@ -0,0 +1,14 @@
+package main;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SimulationTest {
+
+	@Test
+	public void testCheckMinimunList() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
