33f28d353d94a1384df1cd0a508bf76d533bf82c
Javain <javain77@gmail.com>
2015-12-02 00:38:51 +0100

    Mi nombre al documento

--- src/main/Simulation.java
+++ src/main/Simulation.java
@@ -1,5 +1,5 @@
 /**
- * Copyright 2015 Javier Montero,Diego Martin
+ * Copyright 2015 Javier Montero, Diego Martin, Javier Rabanillo
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
